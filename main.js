c06f75dbf75976ac2a6e8bd0953d19958b67695c
taka-0 <taka.08070309@gmail.com>
2021-01-10 22:24:25 +0900

    練習

---
+++ main.js
@@ -0,0 +1,13 @@
+// 'use strict';
+// // エラー発見のため
+
+// console.log('Hello World from main js!');
+
+'use strict';
+
+const score=85;
+
+if(score>80){
+  console.log('合格です。');
+}
+else{cosole.log('不合格です。');}
